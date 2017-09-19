44af5545f57d126cc83550d9365858212968f3b2
patrykfischer <fischer.patryk@gmail.com>
2017-09-19 23:15:51 +0200

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :update] do
+  resources :users, only: [:index, :update], param: :user_id do
     member do
       get 'assigned', to: 'users#assigned'
       get 'own',      to: 'users#own'
-    end
-    resources :tickets, only: [:index, :create] do
-      put '/change_status', to: 'tickets#change_status'
+      resources :tickets, only: [:index, :create] do
+        put '/change_status', to: 'tickets#change_status'
+      end
     end
   end
 end
